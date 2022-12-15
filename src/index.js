280e5782a9b79fa9dbc14a6e329220706ecbfb26
Tatiana <Bowmantatiana2@gmail.com>
2022-12-15 08:38:56 -0800

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form"); // #create-task-form
+ form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const inputDescription  = document.querySelector("form > input#new-task-description")
+    // or  const inputDescription  = document.querySelector(#new-task-description")
+    // or e.target.new-task-description.value
+
+    createListItem(inputDescription.value)
+   form.reset();
+ })
 });
+
+
+function createListItem(toDo) {
+  const li = document.createElement("li");
+  li.textContent = `${toDo}  `
+
+  const btn = document.createElement("button")
+  btn.textContent = "done";
+
+  btn.addEventListener("click", () => {
+    li.remove();
+  })
+
+  li.appendChild(btn);
+  document.querySelector("#tasks").appendChild(li);
+}
+
+
+
